49185b7121ae0dc9e82105620cf02aa76e1107c6
jugaldeb134 <43871677+jugaldeb134@users.noreply.github.com>
2018-12-11 01:31:07 -0600

    Cambios Jorge 1:30 am

--- Proyecto_Final/IndexAdmin.aspx.cs
+++ Proyecto_Final/IndexAdmin.aspx.cs
@@ -9,7 +9,39 @@ public partial class IndexAdmin : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-       
+        if (!IsPostBack)
+        {
+
+            if (Global.userType == 1)
+            {
+                btnSeguridad.Visible = true;
+                btnConsultas.Visible = true;
+                btnAdmin.Visible = true;
+            }
+
+            else if (Global.userType == 2)
+            {
+
+                btnSeguridad.Visible = true;
+                btnConsultas.Visible = false;
+                btnAdmin.Visible = false;
+            }
+
+            else if (Global.userType == 3 || Global.userType == 4)
+            {
+                btnSeguridad.Visible = false;
+                btnConsultas.Visible = false;
+                btnAdmin.Visible = true;
+
+            }
+            else if (Global.userType == 5)
+            {
+                btnSeguridad.Visible = false;
+                btnConsultas.Visible = true;
+                btnAdmin.Visible = false;
+            }
+        }
+    }
     }
 
     protected void btnSeguridad_Click(object sender, EventArgs e)
